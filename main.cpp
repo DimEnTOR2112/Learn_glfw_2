4ec81d77ad6ee02d65b0a6c50dca7be923915ad7
DimEnTOR <UNluke333@yandex.ru>
2022-11-09 20:09:52 -0200

    added glfw and render window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include<iostream>
 
-int main()
+
+int main(void)
 {
-	std::cout<<"hello world"<<std::endl;
-	return 0;
+    GLFWwindow* pWindow;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    pWindow = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!pWindow)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(pWindow);
+
+    if (!gladLoadGL())
+    {
+        std::cout << "cant load glad";
+        return -1;
+    }
+    std::cout << GLVersion.major << "." << GLVersion.minor << std::endl;
+    glClearColor(1,0,0,0);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(pWindow);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
